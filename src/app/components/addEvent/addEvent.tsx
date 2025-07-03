cc1821471b6992f26ecb41f2f93948118282a397
Antoinette Janus <acj.gfx@gmail.com>
2025-07-03 15:01:59 -0700

    Added form submission

--- src/app/components/addEvent/addEvent.tsx
+++ src/app/components/addEvent/addEvent.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
+import { useNavigate } from "react-router";
 import EventInfo from "../../models/event";
 
 export default function AddEvent({ events, setEvents }:{events: EventInfo[], setEvents: Function}){
+    const navigate = useNavigate();
     const generateAvailableTimes = ()=> { 
         let time = 0;
         const times = [0];
@@ -25,15 +28,37 @@ export default function AddEvent({ events, setEvents }:{events: EventInfo[], set
         }
         return times;
     }
-    console.log(events);
     const times = generateAvailableTimes();
-    return <form>
+    const submitForm = async (e: React.FormEvent<HTMLFormElement>)=> {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        let formJson = Object.fromEntries(formData.entries());
+        const parsedStartTime = formJson.startTime.toString().split(':');
+        const newStartTime = new Date();
+        newStartTime.setHours(parseInt(parsedStartTime[0]));
+        newStartTime.setMinutes(parseInt(parsedStartTime[1]));
+        const parsedEndTime = formJson.endTime.toString().split(':');
+        const newEndTime = new Date();
+        newEndTime.setHours(parseInt(parsedEndTime[0]));
+        newEndTime.setMinutes(parseInt(parsedEndTime[1]));
+        const newEntry = {
+            title: formJson.title,
+            startTime: newStartTime,
+            endTime: newEndTime
+        }
+        const newEvents = [...events, newEntry].sort((a, b)=> {
+            return a.startTime.getTime()/1000 < b.startTime.getTime()/1000 ? -1 : 1
+        });
+        await setEvents(newEvents);
+        return navigate('/');
+    }
+    return <form onSubmit={submitForm}>
         <div>
-            <label htmlFor="title">Event:</label><input id="title" placeholder="Example event name"/>
+            <label htmlFor="title">Event:</label><input id="title" name="title" placeholder="Example event name"/>
         </div>
         <div>
             <label htmlFor="start-time">Start time:</label>
-            <select id="start-time">{
+            <select id="start-time" name="startTime">{
                 times.map(time => {
                     const hr = Math.floor(time/60);
                     const mins = (time - (hr * 60));
@@ -47,7 +72,7 @@ export default function AddEvent({ events, setEvents }:{events: EventInfo[], set
         </div>
         <div>
             <label htmlFor="end-time">End time:</label>
-            <select id="end-time">{
+            <select id="end-time" name="endTime">{
                 times.map(time => {
                     const hr = Math.floor(time/60);
                     const mins = (time - (hr * 60));
@@ -59,6 +84,6 @@ export default function AddEvent({ events, setEvents }:{events: EventInfo[], set
                 })
             }</select>
         </div>
-        <button>Add Event</button>
+        <button type="submit">Add Event</button>
     </form>
 }
